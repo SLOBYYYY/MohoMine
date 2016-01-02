35955ca4216776181b4314a420d5d256812217ec
SLOBY <szabolcs.maj@gmail.com>
2016-01-02 20:20:31 +0100

    Added money formatting function

---
+++ web/static/js/formatter.js
@@ -0,0 +1,13 @@
+Number.prototype.formatMoney = function () {
+	var number = this,
+		decimalPlaces = 2,
+		thousandSign = " ",
+		decimalSign = ",",
+		sign = number < 0 ? "-" : "",
+		i = parseInt(number = Math.abs(+number || 0).toFixed(decimalPlaces)) + "",
+		j = (j = i.length) > 3 ? j % 3 : 0;
+		return sign +
+			(j ? i.substr(0, j) + thousandSign : "") +
+			i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousandSign) +
+			(decimalPlaces ? decimalSign + Math.abs(number - i).toFixed(decimalPlaces).slice(2) : "");
+}
