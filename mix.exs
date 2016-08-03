1449078fcbe7a883adb16775c171b994138e7b2b
SLOBY <szabolcs.maj@gmail.com>
2016-08-03 11:54:52 +0200

    Updated config files and template to use newer version of phoenix. Added mix.lock to .gitignore

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MohoMine.Mixfile do
   def project do
     [app: :moho_mine,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -30,10 +30,10 @@ defmodule MohoMine.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
+    [{:phoenix, "~> 1.2.0"},
      {:phoenix_ecto, "~> 1.1"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
+     {:phoenix_html, "~> 2.6.0"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"}]
   end
